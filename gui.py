e410ae3bedc738cd585ba4cc7b0d55557034588b
thuymaiwork0304 <thuymaiwork0304@gmail.com>
2025-11-25 20:52:08 +0700

    l

--- gui.py
+++ gui.py
@@ -11,6 +11,7 @@ def remove_accents(input_str):
     nfkd_form = unicodedata.normalize('NFKD', input_str)
     return "".join([c for c in nfkd_form if not unicodedata.combining(c)])
 
+
 class AttendanceApp:
     def __init__(self, root):
         self.root = root
